c8d72f6a58ef877cc134c7b7ffecad93d6f413cd
UjjwalKumar7209 <ujjwal720946@gmail.com>
2025-09-26 21:34:10 +0530

    Setting up react-router

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='flex justify-center items-center p-3 m-3 text-pink-500 bg-black'>
+      About Page
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center p-3 m-3 text-pink-500 bg-black'>
+      Home Page
+    </div>
+  );
+}
+
+export default Home;
